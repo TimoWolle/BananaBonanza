7ee12f05b0ae5bd8788447dd5fd832383d33b56d
TimoWolle <timosch3@gmx.de>
2023-09-12 15:30:45 +0200

    Fancy shit

--- src/main/java/de/bananabonanza/BananaBonanzaAwsApplication.java
+++ src/main/java/de/bananabonanza/BananaBonanzaAwsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BananaBonanzaAwsApplication {
+public class Webshop-Backend-Springboot {
 
     public static void main(String[] args) {
-        SpringApplication.run(BananaBonanzaAwsApplication.class, args);
+        SpringApplication.run(Webshop-Backend-Springboot.class, args);
     }
 
 }
